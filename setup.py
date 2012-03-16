e70b48a976b37a3221cb3f23d6256e92aeab575a
Legrandin <gooksankoo@hoiptorrow.mailexpire.com>
2012-03-16 12:01:37 +0100

    Updated setup.py with new version (2.2rc1), description, URL, and classifiers

--- setup.py
+++ setup.py
@@ -1,12 +1,20 @@
 from distutils.core import setup
 
 setup(  name='PyAuthenNTLM2',
-        version="2.1",
-        description='A mod-python module for Apache that carries out NTLM authentication',
+        description='PyAuthenNTLM2 is a pure Python module that enables Apache to carry out authentication via NTLM and an external Domain Controller or Active Directory server.',
+        version="2.2rc1",
         author='Legrandin',
         author_email='gooksankoo@hoiptorrow.mailexpire.com',
+        url='https://github.com/Legrandin/PyAuthenNTLM2',
         license='Apache 2.0',
         py_modules=['pyntlm'],
         packages=['PyAuthenNTLM2'],
+        classifiers=[
+            'Development Status :: 4 - Beta',
+            'Programming Language :: Python',
+            'Intended Audience :: System Administrators',
+            'Environment :: Plugins',
+            'Framework :: mod-python',
+        ],
 )
 
