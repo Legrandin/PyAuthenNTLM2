8102aba13cf9953aa51df3bbcdf0ec6f19094780
Guntram Blohm <extern.guntram.blohm@audi.de>
2014-04-02 11:38:39 +0200

    (At least some versions of) Apache will always log APLOG_NOTICE, independent of LogLevel, which produces a lot of spam in error_log. Changed Log Level APLOG_NOTICE to APLOG_INFO to prevent this.

--- pyntlm.py
+++ pyntlm.py
@@ -339,7 +339,7 @@ def handle_type3(req, ntlm_message):
             domain,user,req.unparsed_uri))
         return handle_unauthorized(req)
 
-    req.log_error('PYNTLM: User %s/%s has been authenticated to access URI %s' % (user,domain,req.unparsed_uri), apache.APLOG_NOTICE)
+    req.log_error('PYNTLM: User %s/%s has been authenticated to access URI %s' % (user,domain,req.unparsed_uri), apache.APLOG_INFO)
     set_remote_user(req, user, domain)
     result = check_authorization(req, user, proxy)
     cache.remove(req.connection.id)
@@ -373,7 +373,7 @@ def handle_basic(req, user, password):
             user,domain,req.unparsed_uri))
         return handle_unauthorized(req)
     
-    req.log_error('PYNTLM: User %s/%s has been authenticated (Basic) to access URI %s' % (user,domain,req.unparsed_uri), apache.APLOG_NOTICE)
+    req.log_error('PYNTLM: User %s/%s has been authenticated (Basic) to access URI %s' % (user,domain,req.unparsed_uri), apache.APLOG_INFO)
     set_remote_user(req, user, domain)
     result = check_authorization(req, user, proxy)
     proxy.close()
